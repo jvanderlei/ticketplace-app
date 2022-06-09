0e26d9f9f27f85c8a107cf4f9fda67940534c776
jvanderlei <joaop.vanderlei@gmail.com>
2022-06-08 21:54:53 -0300

    TicketCards

---
+++ src/components/Mols/TicketCard/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import { defaultTheme as theme } from '../../../styles/theme'
+
+export const TicketCardWrapper = styled.div`
+    display: flex;
+    max-width: 20vw;
+`
+
+export const TicketCardImg = styled.div`
+    img {
+        max-width: 250px
+    }
+`
